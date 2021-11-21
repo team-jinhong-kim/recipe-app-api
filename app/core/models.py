c51a6282fd3725c3345d4dca00686a28c520546a
team-jinhong-kim <59462921+team-jinhong-kim@users.noreply.github.com>
2021-11-21 12:08:38 +0900

    Added user module related features and corresponding testings

--- app/core/models.py
+++ app/core/models.py
@@ -8,13 +8,22 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-
+        if not email:
+            raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
+    def create_super_user(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -7,6 +7,7 @@ class ModelTest(TestCase):
         """Test creating a new user with an email is successful"""
         email = 'test@email.com'
         password = 'Testpass123'
+        # usermodel 을 가져온다. user 모델의 objects 는 UserManager(BaseUserManager) 이다. (core/models.py)
         user = get_user_model().objects.create_user(
             email=email,
             password=password
@@ -19,5 +20,21 @@ class ModelTest(TestCase):
         """Test the email for a new user is normalized"""
         email = 'test@LONDONAPPDEV.COM'
         user = get_user_model().objects.create_user(email, 'test123')
+        print(get_user_model().objects)
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpwd')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_super_user(
+            'test@jakeemail.com',
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
